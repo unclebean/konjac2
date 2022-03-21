84b7fee763a793f085da08b14eacc7f1b4eb6ee2
hou jian <unclebean2013@gmail.com>
2022-03-21 23:49:53 +0800

    fixed default settings impact test issue

--- konjac2/config.py
+++ konjac2/config.py
@@ -2,18 +2,18 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    app_name: str
-    run_cron_job: bool
-    oanda_url: str
-    oanda_token: str
-    oanda_account: str
-    units: int
-    ftx_spot_api_key: str
-    ftx_spot_secret: str
-    ftx_future_api_key: str
-    ftx_future_secret: str
-    binance_api_key: str
-    binance_secret: str
+    app_name: str = "konjac2"
+    run_cron_job: bool = False
+    oanda_url: str = ""
+    oanda_token: str = ""
+    oanda_account: str = ""
+    units: int = 5000
+    ftx_spot_api_key: str = ""
+    ftx_spot_secret: str = ""
+    ftx_future_api_key: str = ""
+    ftx_future_secret: str = ""
+    binance_api_key: str = ""
+    binance_secret: str = ""
 
     class Config:
         env_file = ".env"
