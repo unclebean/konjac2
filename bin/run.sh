e6abf35f4da6ab15c031e1e87c93a7e271d08267
hou jian <unclebean2013@gmail.com>
2022-05-11 07:16:46 +0800

    try to fix stop app issue v2

--- bin/run.sh
+++ bin/run.sh
@@ -7,13 +7,11 @@ case $1 in
     start)
         pwd
         # Launch your program as a detached process
-        poetry shell
-        uvicorn konjac2.main:app --host 0.0.0.0 --port 5555 2>/dev/null &
+        poetry run uvicorn konjac2.main:app --host 0.0.0.0 --port 5555 2>/dev/null &
         # Get its PID and store it
         echo $! > ${PIDFILE}
     ;;
     stop)
-        cd "$(dirname "${BASH_SOURCE[0]}")"
         pwd
         # Launch your program as a detached process
         kill -15 `cat ${PIDFILE}`
