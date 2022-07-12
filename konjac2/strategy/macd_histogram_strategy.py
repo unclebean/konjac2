7992020f45ce2413fcf5d423ce962148f2eaf444
hou jian <unclebean2013@gmail.com>
2022-07-12 11:44:29 +0800

    add more trading scenario

--- konjac2/strategy/macd_histogram_strategy.py
+++ konjac2/strategy/macd_histogram_strategy.py
@@ -41,9 +41,14 @@ class MacdHistogramStrategy(ABCStrategy):
             stock_rsi_k = stoch_rsi_data["STOCHRSIk_14_14_3_3"]
             stock_rsi_d = stoch_rsi_data["STOCHRSId_14_14_3_3"]
             if (
-                (macd_histogram[-1] < 0 or (macd_histogram[-2] < 0 and macd_histogram[-1] > 0))
+                ((macd_histogram[-1] < 0 or (macd_histogram[-2] < 0 and macd_histogram[-1] > 0))
                 and macd_histogram[-2] < macd_histogram[-1]
-                and stock_rsi_k[-1] > stock_rsi_d[-1]
+                and stock_rsi_k[-1] > stock_rsi_d[-1])
+                or (macd_histogram[-2] > 0 
+                    and macd_histogram[-1] > 0 
+                    and macd_histogram[-2] < macd_histogram[-1]
+                    and  stock_rsi_k[-1] > stock_rsi_d[-1] 
+                    and  stock_rsi_k[-2] <= stock_rsi_d[-2])
             ):
                 return self._update_open_trade(
                     TradeType.long.name, candles.close[-1], "macd_ichimoku", macd_histogram[-1], candles.index[-1]
@@ -59,9 +64,14 @@ class MacdHistogramStrategy(ABCStrategy):
             stock_rsi_k = stoch_rsi_data["STOCHRSIk_14_14_3_3"]
             stock_rsi_d = stoch_rsi_data["STOCHRSId_14_14_3_3"]
             if (
-                (macd_histogram[-1] > 0 or (macd_histogram[-2] > 0 and macd_histogram[-1] < 0))
+                ((macd_histogram[-1] > 0 or (macd_histogram[-2] > 0 and macd_histogram[-1] < 0))
                 and macd_histogram[-2] > macd_histogram[-1]
-                and stock_rsi_k[-1] < stock_rsi_d[-1]
+                and stock_rsi_k[-1] < stock_rsi_d[-1])
+                or (macd_histogram[-2] < 0 
+                    and macd_histogram[-1] < 0 
+                    and macd_histogram[-2] > macd_histogram[-1]
+                    and  stock_rsi_k[-1] < stock_rsi_d[-1] 
+                    and  stock_rsi_k[-2] >= stock_rsi_d[-2])
             ):
                 return self._update_open_trade(
                     TradeType.short.name, candles.close[-1], "macd_ichimoku", macd_histogram[-1], candles.index[-1]
