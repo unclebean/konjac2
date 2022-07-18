45edf4ce907d38ae8b63a94fd0bcca932fe0bcf5
hou jian <unclebean2013@gmail.com>
2022-07-18 12:09:37 +0800

    revert continue flowing the trend

--- konjac2/strategy/macd_histogram_strategy.py
+++ konjac2/strategy/macd_histogram_strategy.py
@@ -47,8 +47,8 @@ class MacdHistogramStrategy(ABCStrategy):
                     or (0 < macd_histogram[-3] > macd_histogram[-2] and macd_histogram[-2] * 2 < macd_histogram[-1] > 0
                         and stock_rsi_k[-1] > stock_rsi_d[-1]
                         and stock_rsi_k[-2] <= stock_rsi_d[-2])
-                    or (0 < macd_histogram[-3] < macd_histogram[-2] < macd_histogram[-1]
-                        and stock_rsi_k[-1] > stock_rsi_d[-1])
+                    # or (0 < macd_histogram[-3] < macd_histogram[-2] < macd_histogram[-1]
+                    #     and stock_rsi_k[-1] > stock_rsi_d[-1])
             ):
                 return self._update_open_trade(
                     TradeType.long.name, candles.close[-1], "macd_ichimoku", macd_histogram[-1], candles.index[-1]
@@ -70,8 +70,8 @@ class MacdHistogramStrategy(ABCStrategy):
                     or (0 > macd_histogram[-3] < macd_histogram[-2] and macd_histogram[-2] * 2 > macd_histogram[-1] < 0
                         and stock_rsi_k[-1] < stock_rsi_d[-1]
                         and stock_rsi_k[-2] >= stock_rsi_d[-2])
-                    or (0 > macd_histogram[-3] > macd_histogram[-2] > macd_histogram[-1]
-                        and stock_rsi_k[-1] < stock_rsi_d[-1])
+                    # or (0 > macd_histogram[-3] > macd_histogram[-2] > macd_histogram[-1]
+                    #     and stock_rsi_k[-1] < stock_rsi_d[-1])
             ):
                 return self._update_open_trade(
                     TradeType.short.name, candles.close[-1], "macd_ichimoku", macd_histogram[-1], candles.index[-1]
