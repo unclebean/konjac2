7dc07150ddc3df1f8265021ef621c39949e4d1f7
hou jian <unclebean2013@gmail.com>
2022-08-05 22:22:55 +0800

    trading with M30

--- konjac2/jobs/scanner.py
+++ konjac2/jobs/scanner.py
@@ -86,7 +86,7 @@ async def smart_bot(currency="SAND"):
     query_symbol = f"{currency}-PERP"
     trade_symbol = f"{currency}-PERP"
     strategy = LogisticRegressionStrategy(symbol=query_symbol)
-    data = fetch_data(query_symbol, "H1", True, limit=1500)
+    data = fetch_data(query_symbol, "M30", True, limit=1500)
     d_data = fetch_data(query_symbol, "D", False, counts=1500)
 
     opened_position = opened_position_by_symbol(trade_symbol)
