9694fd808778daef70ad967454826d1960d238d5
hou jian <unclebean2013@gmail.com>
2022-09-07 09:39:04 +0800

    update backtest

--- konjac2/backtest/backtest.py
+++ konjac2/backtest/backtest.py
@@ -10,17 +10,23 @@ from konjac2.strategy.vegas_strategy import VegasStrategy
 from konjac2.strategy.logistic_regression_strategy import LogisticRegressionStrategy
 from .prepare_data import prepare_forex_backtest_data
 from ..indicator.heikin_ashi_momentum import heikin_ashi_mom
+from ..indicator.utils import resample_to_interval
 from ..models import apply_session
 from ..models.trade import Trade
 from ..strategy.bb_stoch_strategy import BBStochStrategy
 from ..strategy.cci_histogram_strategy import CCIHistogramStrategy
+from ..strategy.ce_rsi_strategy import CERSIStrategy
+from ..strategy.ema_stoch_rsi_strategy import EmaStochRsiStrategy
 from ..strategy.ichimoku_willr_strategy import IchimokuWillR
 from ..strategy.macd_rsi_strategy import MacdRsiStrategy
 from ..strategy.macd_rsi_vwap_strategy import MacdRsiVwapStrategy
 from ..strategy.n_macd_volatility_strategy import NMacdVolatilityStrategy
+from ..strategy.smoothed_ha_strategy import SmoothedHAStrategy
 from ..strategy.strategy_five import StrategyFive
 from ..strategy.strategy_one import StrategyOne
+from ..strategy.vix_strategy import VixStrategy
 from ..strategy.vwap_rsi_strategy import VwapRsiStrategy
+from ..strategy.vwap_rsi_willr_strategy import VwapRsiWillR
 
 logging.basicConfig()
 logging.getLogger("sqlalchemy").setLevel(logging.ERROR)
@@ -57,44 +63,13 @@ def short_term_backtest(symbol: str):
     trades.delete(synchronize_session=False)
     session.commit()
     session.close()
-    h1_data = pd.read_csv(f"{symbol}_1_0.csv", index_col="date", parse_dates=True) #.loc["2021-03-20 00:00:00":]
-    d_data = pd.read_csv(f"{symbol}_4_0.csv", index_col="date", parse_dates=True)
-    strategy = IchimokuWillR(symbol=symbol)
+    h1_data = pd.read_csv(f"{symbol}_0_5.csv", index_col="date", parse_dates=True).loc["2019-08-01 00:00:00":]
+    strategy = BBCCIStrategy(symbol=symbol)
     for window in h1_data.rolling(window=999):
         if len(window.index) < 999:
             continue
-        # last_day = window.index[-1].strftime("%Y-%m-%d")
-        last_index = window.index[-1]
 
-        if 4 <= last_index.hour < 8:
-            last_h4_index = last_index.strftime("%Y-%m-%d") + " 00:00:00"
-        elif 8 <= last_index.hour < 12:
-            last_h4_index = last_index.strftime("%Y-%m-%d") + " 04:00:00"
-        elif 12 <= last_index.hour < 16:
-            last_h4_index = last_index.strftime("%Y-%m-%d") + " 08:00:00"
-        elif 16 <= last_index.hour < 20:
-            last_h4_index = last_index.strftime("%Y-%m-%d") + " 12:00:00"
-        elif 20 <= last_index.hour <= 23:
-            last_h4_index = last_index.strftime("%Y-%m-%d") + " 16:00:00"
-        else:
-            last_h4_index = (last_index - timedelta(days=1)).strftime("%Y-%m-%d") + " 20:00:00"
-        # print(window)
-        current_day_data = d_data.loc[:last_h4_index]
-        # near_real_time_candle = window.loc[last_h4_index:last_index][4:]
-        # #
-        # if len(near_real_time_candle) > 0:
-        #     open_price = near_real_time_candle.open[0]
-        #     close_price = near_real_time_candle.close[-1]
-        #     high_price = near_real_time_candle.high.max()
-        #     low_price = near_real_time_candle.low.min()
-        #     volume = near_real_time_candle.volume.sum()
-        #     near_real_time_row = pd.DataFrame([[open_price, close_price, high_price, low_price, volume]], columns=["open", "close", "high", "low", "volume"], index=[last_index])
-        #     current_day_data = current_day_data.append(near_real_time_row)
-
-        # day_index = (last_index - timedelta(days=1)).strftime("%Y-%m-%d") + " 00:00:00"
-        # d_candles = d_data.loc[:day_index]
-
-        # print(current_day_data)
+        current_day_data = resample_to_interval(window, 60)
         strategy.exit_signal(window, current_day_data)
         strategy.seek_trend(window, current_day_data)
         strategy.entry_signal(window, current_day_data)
@@ -119,7 +94,7 @@ def fx_short_term_backtest(symbol: str):
     trades.delete(synchronize_session=False)
     session.commit()
     session.close()
-    m5_data = pd.read_csv(f"{symbol}_1_0.csv", index_col="date", parse_dates=True).loc["2020-01-20 00:00:00":]
+    m5_data = pd.read_csv(f"{symbol}_1_0.csv", index_col="date", parse_dates=True) #.loc["2022-03-20 00:00:00":]
     h4_data = pd.read_csv(f"{symbol}_4_0.csv", index_col="date", parse_dates=True)
     strategy = IchimokuWillR(symbol=symbol)
     for window in m5_data.rolling(window=999):
@@ -156,3 +131,8 @@ def fx_short_term_backtest(symbol: str):
     session.close()
     print(strategy.balance)
     return total_result.scalar()
+
+
+def zt(window):
+    print(window)
+    return window
