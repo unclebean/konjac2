69dc709d26a7aba60dc8b00831a48d58693a4a96
hou jian <unclebean2013@gmail.com>
2022-09-15 21:51:10 +0800

    trade crypto using ichimoku strategy

--- konjac2/jobs/scanner.py
+++ konjac2/jobs/scanner.py
@@ -91,9 +91,9 @@ async def bbcci_scanner():
 async def smart_bot(currency="SAND"):
     query_symbol = f"{currency}-PERP"
     trade_symbol = f"{currency}-PERP"
-    strategy = DemaSuperTrendStrategy(symbol=query_symbol)
-    data = fetch_data(query_symbol, "M5", True, limit=1500)
-    d_data = resample_to_interval(data, 60)
+    strategy = IchimokuWillR(symbol=query_symbol)
+    data = fetch_data(query_symbol, "H1", True, limit=1500)
+    d_data = resample_to_interval(data, 240)
     # d_data = fetch_data(query_symbol, "H4", True, counts=1500)
 
     opened_position = opened_position_by_symbol(trade_symbol)
@@ -197,10 +197,12 @@ async def place_crypto_order(symbol: str, trend: str):
 
 
 async def scanner_job():
-    await asyncio.sleep(30)
-    await scan_crypto()
+    pass
+    # await asyncio.sleep(30)
+    # await scan_crypto()
 
 
 async def scanner_h1_job():
     await asyncio.sleep(30)
     await scan_forex()
+    await scan_crypto()
