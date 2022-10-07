5f1258e083002e6cdb64d9b9332992af62fa5092
hou jian <unclebean2013@gmail.com>
2022-10-07 10:45:08 +0800

    update gain rate

--- konjac2/service/crypto/fetcher.py
+++ konjac2/service/crypto/fetcher.py
@@ -119,25 +119,25 @@ def open_position(symbol, tradeType, tp=0, sl=0, loss_position=None):
     balance = get_ftx_balance()
     log.info("open position for {} current balance {}".format(symbol, balance))
     price = ftx_fetcher(symbol, "M15", complete=False)[-1:]["close"].values[0]
-    amount = balance / price
+    amount = balance / price * 10
     side = "buy" if tradeType == "long" else "sell"
     exchange.cancel_all_orders(symbol)
     exchange.create_market_order(symbol, side, amount)
     quantity_price = amount * price
-    gain_rate = 0.020 if tp == 0 else tp
+    gain_rate = 0.0055 if tp == 0 else tp
     loss_rate = 0.020 if sl == 0 else sl
     if side == "buy":
         gain = (quantity_price + quantity_price * gain_rate) / amount
         loss = (quantity_price - quantity_price * loss_rate) / amount
         loss_price = loss_position if loss_position is not None else loss
         exchange.create_order(symbol, "takeProfit", "sell", amount, None, params={"triggerPrice": gain})
-        exchange.create_order(symbol, "stop", "sell", amount, None, params={"triggerPrice": loss_price})
+        # exchange.create_order(symbol, "stop", "sell", amount, None, params={"triggerPrice": loss_price})
     else:
         gain = (quantity_price - quantity_price * gain_rate) / amount
         loss = (quantity_price + quantity_price * loss_rate) / amount
         loss_price = loss_position if loss_position is not None else loss
         exchange.create_order(symbol, "takeProfit", "buy", amount, None, params={"triggerPrice": gain})
-        exchange.create_order(symbol, "stop", "buy", amount, None, params={"triggerPrice": loss_price})
+        # exchange.create_order(symbol, "stop", "buy", amount, None, params={"triggerPrice": loss_price})
 
 
 def close_position(symbol):
