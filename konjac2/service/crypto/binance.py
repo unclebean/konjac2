e6ca9f77f8afb1af82be2ad27130c18d4570f3a6
hou jian <unclebean2013@gmail.com>
2022-12-18 14:17:34 +0800

    fixed close position issues

--- konjac2/service/crypto/binance.py
+++ konjac2/service/crypto/binance.py
@@ -46,7 +46,7 @@ def close_position(symbol):
     positions = exchange.fetch_positions()
     symbol_position = next(p for p in positions if p["symbol"] == symbol)
     side = symbol_position["side"]
-    if side == "buy":
+    if side == "long":
         exchange.create_market_sell_order(symbol, float(symbol_position["contracts"]))
     else:
         exchange.create_market_buy_order(symbol, float(symbol_position["contracts"]))
