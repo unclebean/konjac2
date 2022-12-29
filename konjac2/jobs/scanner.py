2275b7cbce07df1557e4016abe11a3222aa58856
hou jian <unclebean2013@gmail.com>
2022-12-29 22:29:57 +0800

    change back to gemini api for fetching data

--- konjac2/jobs/scanner.py
+++ konjac2/jobs/scanner.py
@@ -21,7 +21,7 @@ log = logging.getLogger(__name__)
 
 
 async def smart_bot(currency="ETH"):
-    query_symbol = f"{currency}/BUSD"
+    query_symbol = f"{currency}/USD"
     spot_symbol = f"{currency}/USD"
     future_symbol = f"{currency}/USDT"
     strategy = UTBotStrategy(symbol=spot_symbol)
