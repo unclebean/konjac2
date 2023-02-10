18858fded9afa69a8d06a53d97332a8492f03ce2
hou jian <unclebean2013@gmail.com>
2023-02-10 21:26:21 +0800

    add trading view webhook

--- konjac2/routers/daily_trend.py
+++ konjac2/routers/daily_trend.py
@@ -1,10 +1,25 @@
+import logging
+
 from fastapi import APIRouter
+from pydantic import BaseModel
+
 from konjac2.service.trender import get_today_trenders
 
+log = logging.getLogger(__name__)
 router = APIRouter()
+
+class TradeSignal(BaseModel):
+    symbol: str
+    signal: str
+    action: str
 
 
 @router.get("/trends", tags=["trend"])
 async def get_daily_trend():
     daily_trends = get_today_trenders()
     return daily_trends
+
+
+@router.post("/trading_view/signal", tags=["trend"])
+async def receive_trading_view_signal(trade_signal: TradeSignal):
+    log.info(f'symbol: {trade_signal.symbol} signal: {trade_signal.signal} action: {trade_signal.action}')
