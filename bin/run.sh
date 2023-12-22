c0a43d5c986f9479bd8e7bb10ab632a28bf53318
unclebean <unclebean2013@gmail.com>
2023-12-22 14:29:49 +0800

    change start app script

--- bin/run.sh
+++ bin/run.sh
@@ -7,7 +7,7 @@ case $1 in
     start)
         pwd
         # Launch your program as a detached process
-        poetry run uvicorn konjac2.main:app --host 0.0.0.0 --port 5555 2>/dev/null &
+        poetry run uvicorn konjac2.main:app --host 0.0.0.0 --port 5555
         # Get its PID and store it
         echo $! > ${PIDFILE}
     ;;
